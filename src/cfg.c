dc23dd6f13caec8d7b52153cc0764dfc92b889b5
Raghuram Subramani <raghus2247@gmail.com>
2024-06-11 21:02:14 +0530

    Use macro for a simple function

--- src/cfg.c
+++ src/cfg.c
@@ -5,9 +5,7 @@
 
 #include "../include/cfg.h"
 
-static int format_string_length(const char* string1, const char* string2) {
-  return strlen(string1) + strlen(string2) + 1;
-}
+#define FORMAT_STRING_LENGTH(x, y) strlen(x) + strlen(y) + 1
 
 static size_t size_of_null_terminated_array(CfgVariable** array) {
     size_t i = 0;
@@ -27,7 +25,7 @@ static char* format(const CfgVariable* variable) {
       break;
   }
 
-  size_t new_length = format_string_length(variable->name, format_specifier);
+  size_t new_length = FORMAT_STRING_LENGTH(variable->name, format_specifier);
   char* formatted = calloc(new_length, sizeof(char));
 
   strcpy(formatted, variable->name);
