9ee45e48efd11cc476f1d5446ee7d02189ebd472
Raghuram Subramani <raghus2247@gmail.com>
2024-06-11 21:08:51 +0530

    Re-order

--- examples/example.c
+++ examples/example.c
@@ -4,12 +4,12 @@
 
 CfgVariable* variables[] = {
   &(CfgVariable) {
-    "port",
-    INTEGER
-  },
-  &(CfgVariable) {
     "address",
     STRING
+  },
+  &(CfgVariable) {
+    "port",
+    INTEGER
   }
 };
 
@@ -17,11 +17,10 @@ int main(int argc, char** argv) {
   (void) argc;
   (void) argv;
 
-
   void** results = parse("example.cfg", variables);
 
-  int* port = results[0];
-  char* address = results[1];
+  char* address = results[0];
+  int* port = results[1];
 
   printf("address = %s\n", address);
   printf("port = %d\n", *port);
